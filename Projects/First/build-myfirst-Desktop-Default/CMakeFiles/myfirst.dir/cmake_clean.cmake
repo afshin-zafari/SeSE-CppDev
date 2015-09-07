d46cf68faa1ee4c5698578126a1bcc49085c618e
cppdev <afshin.zafari@it.uu.se>
2015-09-07 11:03:04 +0200

    First Try.

---
+++ Projects/First/build-myfirst-Desktop-Default/CMakeFiles/myfirst.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myfirst.dir/main.c.o"
+  "myfirst.pdb"
+  "myfirst"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myfirst.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
